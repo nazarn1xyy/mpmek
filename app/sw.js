7f9c03627bb2b8bfd10489358235bdd95c3857fc
Назар Шикір <nazariyshikircloud.com@MacBook-Air-Nazar.local>
2026-04-15 23:30:56 +0300

    fix: remove clients.claim() from SW to prevent page hijack jerk on first load

--- app/sw.js
+++ app/sw.js
@@ -23,7 +23,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME && k !== NOTIF_CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    )
   );
 });
 
